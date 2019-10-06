0ed4cbc037d1219eb29ae892a325366ab551782b
linhan <unitimothy@gmail.com>
2019-10-06 10:57:44 -0400

    add turbolink to each page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# User.create(email: 'admin@mail.com', password: 'password')
+User.create(email: 'admin@mail.com', password: 'password')
 DayOfWeek.create(day_of_week: "Monday", day_of_business_hour_attributes: {open_at: DateTime.now.change({ hour: 16, min: 0, sec: 0 }), close_at: DateTime.now.change({ hour: 22, min: 0, sec: 0 })})
 DayOfWeek.create(day_of_week: "Tuesday", day_of_business_hour_attributes: {open_at: DateTime.now.change({ hour: 16, min: 0, sec: 0 }), close_at: DateTime.now.change({ hour: 22, min: 0, sec: 0 })}) 
 DayOfWeek.create(day_of_week: "Wednesday", day_of_business_hour_attributes: {open_at: DateTime.now.change({ hour: 16, min: 0, sec: 0 }), close_at: DateTime.now.change({ hour: 22, min: 0, sec: 0 })})  
